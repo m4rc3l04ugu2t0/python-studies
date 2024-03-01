4859a804696b7cac9792eef018dcf4d4cda7b7b7
debian <debian@debian.com.br>
2024-02-29 23:58:16 -0300

    func print

---
+++ function-print/print.py
@@ -0,0 +1,12 @@
+# print("code")
+# print("code", "code1")
+# print(123, 456)
+
+# set the end parameter
+print("code", end="\n")
+print("code1", end="  ")
+print(123, 456)
+
+# separator
+print("code", "code1", sep=", ")
+print(123, 456, sep=", ")
